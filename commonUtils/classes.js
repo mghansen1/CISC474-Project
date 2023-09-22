7de2c61159cc2ef2765668119a2fee7a97e65be2
Matt <mghansen@udel.edu>
2023-09-22 01:41:30 -0400

    added attack hitbox

--- commonUtils/classes.js
+++ commonUtils/classes.js
@@ -1,47 +1,74 @@
 class Sprite {
-    constructor({position}) {
+    constructor({position, size}) {
         this.position = position
-        this.width = 50
-        this.height = 150
+        this.size = size
     }
 
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)    
+        c.fillRect(this.position.x, this.position.y, this.size.width, this.size.height)    
+    }
+
+    animateFrame() {
+        //cycle through sprites
     }
 }
 
 class Fighter extends Sprite {
     constructor({
         velocity,
-        position
+        position,
+        attackArea,
+        size,
+        facingDirection
     }) {
-        super({position})
+        super({position, size})
         this.velocity = velocity
-        this.width = 50
-        this.height = 150
         this.lastkey
+        this.attackArea = attackArea
+        this.isAttacking = false
+        this.facingDirection = facingDirection
+        this.sprite
     }
 
     update() {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        if (this.position.x + this.width + this.velocity.x > canvas.width) {
-            this.position.x = canvas.width - this.width
+        if (this.position.x + this.size.width + this.velocity.x > canvas.width) {
+            this.position.x = canvas.width - this.size.width
             this.velocity.x = 0
         } else if (this.position.x + this.velocity.x < 0) {
             this.position.x = 0
             this.velocity.x = 0
         }
-
-        if (this.position.y + this.height + this.velocity.y > canvas.height) {
-            this.position.y = canvas.height - this.height
+        if (this.position.y + this.size.height + this.velocity.y > canvas.height) {
+            this.position.y = canvas.height - this.size.height
         } else {
             this.velocity.y += gravity
         }
 
         this.draw()
+        
+
+        // depreciate when added attack animation, call animateFrame()
+        if (this.isAttacking) {
+            c.fillStyle = 'green';
+            if (this.facingDirection === 1) {
+                c.fillRect(this.position.x, this.position.y, this.attackArea.width, this.attackArea.height)
+            } else {
+                c.fillRect(this.position.x + this.size.width, this.position.y, -this.attackArea.width, this.attackArea.height)
+            }
+            
+        }
+
     }
+
+    takeDamage() {}
+
+    switchSprite() {
+        // switch through idle, attack, dodge, run, jump, death sprites
+    }
+
 
 }
